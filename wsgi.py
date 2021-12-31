6fea6f41103f6debd7cc3431e48345e38549afac
lamonkey <88therisingsun@gmail.com>
2021-12-30 19:25:40 -0500

    try to fix deployment

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,8 @@
 """
-WSGI config for Portfolio project.
-
+WSGI config for spConverter project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
