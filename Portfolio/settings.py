44fa911d93c5622e775edf663cd573955857d402
lamonkey <88therisingsun@gmail.com>
2022-02-04 21:27:23 -0500

    changed acl config

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -141,18 +141,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 django_heroku.settings(locals())
 
 #set up s3
-# AWS_S3_FILE_OVERWRITE = 'False'
+
 # AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 # AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-# DEFAULT_FILE_STORAGE = os.getenv('DEFAULT_FILE_STORAGE')
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 # AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
-# AWS_DEFAULT_ACL = "None"
+
+AWS_DEFAULT_ACL = None
 AWS_S3_FILE_OVERWRITE = 'False'
 AWS_ACCESS_KEY_ID = 'AKIARQEQ7A25WOOD56MS'
 AWS_SECRET_ACCESS_KEY = '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'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'lamonkey-portfolio-static'
 AWS_S3_REGION_NAME = 'us-east-1'
-AWS_DEFAULT_ACL = "None"
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
