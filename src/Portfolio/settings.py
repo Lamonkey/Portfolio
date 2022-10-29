ea368fce7566bee01cf6d89ab3cc58b8a07a8c48
lamonkey <88therisingsun@gmail.com>
2022-10-28 22:24:34 -0400

    add heroku to allowed host

--- src/Portfolio/settings.py
+++ src/Portfolio/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "mysterious-crag-09703.herokuapp.com"]
-
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "mysterious-crag-09703.herokuapp.com/*"]
 
 # Application definition
 
