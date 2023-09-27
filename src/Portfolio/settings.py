c0c68b13bc306bac188c5259635b33e64498d6b6
lamonkey <88therisingsun@gmail.com>
2023-09-27 17:35:04 -0400

    change defualt mode to production

--- src/Portfolio/settings.py
+++ src/Portfolio/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('MODE', 'development') == 'development'
+DEBUG = os.getenv('MODE', 'production') == 'development'
 
 ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
 CSRF_TRUSTED_ORIGINS = [os.getenv("CSRF_TRUSTED_ORIGINS")]
