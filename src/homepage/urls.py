61d08c0bf5a8557f3a804eae05ec76f001b86a8a
lamonkey <88therisingsun@gmail.com>
2025-11-13 09:47:28 -0500

    remove try catch

--- src/homepage/urls.py
+++ src/homepage/urls.py
@@ -2,13 +2,10 @@ from django.urls import path
 from . import views
 
 app_name = "homepage"
-try:
-    urlpatterns = [
-        path("oldVersion/", views.index, name="index"),
-        path("writing-room/", views.writing_room, name="writing-room"),
-        path("", views.software_log, name="software-log"),
-        path("software-log/<slug:slug>/", views.software_log_detail,
-             name="software-log-detail"),
-    ]
-except Exception as e:
-    print(e)
+
+urlpatterns = [
+    path("oldVersion/", views.index, name="index"),
+    path("writing-room/", views.writing_room, name="writing-room"),
+    path("", views.software_log, name="software-log"),
+    path("software-log/<slug:slug>/", views.software_log_detail, name="software-log-detail"),
+]
